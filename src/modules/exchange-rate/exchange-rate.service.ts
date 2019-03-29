8cb572100ccec6a752e95fbad003af03333b4619
Moemen <moemen.a1@gmail.com>
2019-03-29 16:05:20 +0400

    exchange-rate service module - fix

--- src/modules/exchange-rate/exchange-rate.service.ts
+++ src/modules/exchange-rate/exchange-rate.service.ts
@@ -12,12 +12,12 @@ export class ExchangeRateService {
     }
 
     getExchangeRate(): Observable<any> {
-      return this.apiSrv.get(this.config.ms.latest);
+      const latestEndPoint = this.config.ms.latest.base;
+      return this.apiSrv.get(latestEndPoint);
     }
 
     getExchangeRateHistory(params: ExchangeRateHistoryParams): Observable<any> {
-      const historyEndPoint = this.config.ms.history;
-      const paramsEndPoint = this.config.ms.history.params;
+      const historyEndPoint = this.config.ms.history.base;
       return this.apiSrv.get(historyEndPoint, params)
     }
 
